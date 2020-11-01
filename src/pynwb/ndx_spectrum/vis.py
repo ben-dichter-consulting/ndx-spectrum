2b0f15559c6fc6a658969009e8e3130fd55553fa
Saksham Sharda <saksham20.sharda@gmail.com>
2020-11-01 19:48:11 +0530

    vis fixes

--- src/pynwb/ndx_spectrum/vis.py
+++ src/pynwb/ndx_spectrum/vis.py
@@ -1,28 +1,38 @@
 import matplotlib.pyplot as plt
+from collections.abc import Iterable
+from h5py import Dataset
+
+
+def _data_to_array(data):
+    if isinstance(data, Dataset):
+        return data[()]
+    else:
+        return data
 
 
 def show_spectrum(spectrum):
     naxes = 0
-    if 'power' in spectrum:
+    if 'power' in spectrum.fields:
         naxes += 1
-    if 'phase' in spectrum:
+    if 'phase' in spectrum.fields:
         naxes += 1
 
     fig, axs = plt.subplots(naxes, 1, sharex=True)
 
     caxes = 0
-    ax = axs[caxes]
-    if 'power' in spectrum:
-        ax.semilogy(spectrum.frequencies, spectrum.power)
-        ax.set_ylabel('Power')
+    if not isinstance(axs, Iterable):
+        ax = [axs]
+    for ax_ in ax:
+        if 'power' in spectrum.fields:
+            ax_.semilogy(_data_to_array(spectrum.frequencies), _data_to_array(spectrum.power))
+            ax_.set_ylabel('Power')
 
-        caxes += 1
+            caxes += 1
 
-    if 'phase' in spectrum:
-        ax.plot(spectrum.frequencies, spectrum.phase)
-        ax.set_ylabel('phase')
+        if 'phase' in spectrum.fields:
+            ax_.plot(_data_to_array(spectrum.frequencies), _data_to_array(spectrum.phase))
+            ax_.set_ylabel('phase')
 
-    ax.set_xlabel('frequency')
+        ax_.set_xlabel('frequency')
 
     return fig
-
